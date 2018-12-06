55ed2975a92ef682f56596ee5f47a2d3fa7e111e
P1216442213 <fx_ra@hotmail.com>
2018-12-06 09:47:53 +0000

    commit mytestdummy

--- controller/myView.controller.js
+++ controller/myView.controller.js
@@ -6,7 +6,8 @@ sap.ui.define([
 	return Controller.extend("com.ramesh.controller.myView", {
 checkPresss : function(){
 	
-	
+	console.log("chckPress");
+	console.log("changes in mytestDummy Branch");
 } 
 	});
 });
